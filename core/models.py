f4d8e4c8cdc152d9f933fc41053a9030e88f5619
Grzegorz-T <grzesiu105@gmail.com>
2026-01-02 19:16:48 +0100

    charts.js + dark mode in css

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    github_url = models.URLField(blank=True)
+    live_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
--- core/views.py
+++ core/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from .models import Project
+from django.db.models.functions import TruncMonth
+from django.db.models import Count
 
 def home(request):
-    return render(request, "core/home.html")
+    projects = Project.objects.all().order_by('-created_at')
+    return render(request, "core/home.html", {
+        "projects": projects
+    })
+
+
+def dashboard(request):
+    data = (
+        Project.objects
+        .annotate(month=TruncMonth('created_at'))
+        .values('month')
+        .annotate(count=Count('id'))
+        .order_by('month')
+    )
+
+    labels = [item['month'].strftime('%Y-%m') for item in data]
+    counts = [item['count'] for item in data]
+
+    return render(request, "core/dashboard.html", {
+        "labels": labels,
+        "counts": counts
+    })
